432a454a242bf2c0375f9464bd913c9e08d48a5a
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-14 20:49:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,9 @@ def reverse_each_word(string)
   # reverse = []
   
   # new_array.each { |words| reverse << words.reverse }
-  
+    new_array.collect { |words| words.reverse }
+     
+     
   # reverse.join(" ")
   
   new_array.collect { |words| words.reverse }
