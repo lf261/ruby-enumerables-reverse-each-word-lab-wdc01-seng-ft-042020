6d0b28841b03470e65d76f2d606c9a7d9d5063f1
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-14 20:47:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
   
-  new_array = string.split(" ")
-  reverse = []
+  # new_array = string.split(" ")
+  # reverse = []
   
-  new_array.each { |words| reverse << words.reverse }
+  # new_array.each { |words| reverse << words.reverse }
   
-  reverse.join(" ")
+  # reverse.join(" ")
   
 end 
